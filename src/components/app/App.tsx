a60eca08e5f8550750fee993be54802be6b530b6
sg.sharov <sg.sharov@tensor.ru>
2022-04-15 00:52:02 +0300

    feat: добавил хеддер без поиска

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from '../Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <div className='App__context'>app</div>
+      <div className='App__context'>
+        <Header />
+      </div>
     </div>
   );
 }
