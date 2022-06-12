fcd34415cec9ec732c1dd97733d9172b854e2b55
sg.sharov <sg.sharov@tensor.ru>
2022-06-13 00:23:48 +0300

    feat: реализовал получение данных о стоимости и выручке компании, добавил функции форматирования записей значений стоимости и выручки

--- src/utils/APICompanies.ts
+++ src/utils/APICompanies.ts
@@ -1,13 +1,28 @@
-import {ICompany} from './interfaces';
+import {ICompany, IBisinessCost} from './interfaces';
+
+
 export const BASE_URL = 'http://localhost:4000';
 
-export interface IResponce {
+/*export interface IResponce {
   status: boolean;
   data: any;
-}
+}*/
 
 export const getCompniesList = (): Promise<ICompany[]> => {
 	return fetch(`${BASE_URL}/req`, {
+		mode: 'cors',	
+		method: 'GET',
+	})
+		.then((res) => {
+			if (res.ok) {
+				return res.json();
+			}
+		});
+}
+
+//export const getBisinessCost = (inn: string): Promise<{}> => {
+export const getDataBisinessCost = (inn: string): Promise<IBisinessCost[]> => {
+	return fetch(`${BASE_URL}/business/${inn}`, {
 		mode: 'cors',	
 		method: 'GET',
 	})
