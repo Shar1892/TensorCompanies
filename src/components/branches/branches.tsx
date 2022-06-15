bbf753db6677a40eb1e132f4becef9e0880feb65
sg.sharov <sg.sharov@tensor.ru>
2022-06-16 00:27:47 +0300

    feat: реализовал получение данных по филиалам

--- src/components/branches/branches.tsx
+++ src/components/branches/branches.tsx
@@ -1,4 +1,26 @@
-function Branches() {
+import {useEffect, useState} from 'react';
+
+import {getBranches} from '../../utils/APICompanies';
+
+function Branches(
+  {
+    currentSection,
+    inn,
+  }:
+  {
+    currentSection: string;
+    inn: string;
+  }
+) {
+
+  useEffect(() => {
+    if (inn && currentSection === 'branches') {
+      getBranches(inn).then((data) => {
+        console.log(data);
+        
+      });
+    }
+  }, [inn, currentSection]);
 
   return (
     <div>
